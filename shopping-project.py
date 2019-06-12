30e33eac21f19f959590e97d5132942525d95e66
Annie Kong <annie.kong@stern.nyu.edu>
2019-06-12 15:10:47 -0400

    Looked up products given identifier

--- shopping-project.py
+++ shopping-project.py
@@ -28,9 +28,16 @@ products = [
 # pprint(products)
 
 #INFO CAPTURE / INPUT
-selected_id = input("Please input a product identifier:") #> "g" (string)
-print(selected_id)
-print(type(selected_id))
+selected_id = input("Please input a product identifier:") #> "9" (string)
+matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+matching_product = matching_products[0]
+print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+
+# print(matching_product)
+# print(type(matching_product))
+# print(selected_id)
+# print(type(selected_id))
 
 #INFO DISPLAY/ OUTPUT
 
